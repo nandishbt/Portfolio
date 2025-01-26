43395bf8e140e29ff822b26e994ae619180c7c82
nandishbt <nandishbt2001@gmail.com>
2025-01-26 19:15:27 +0530

    fixed meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,73 +20,73 @@ class MyDocument extends Document {
           <title>Nandish Tippashetty</title>
           <meta
             name="description"
-            content="An enthusiastic front-end software engineer with more than 4 years of experience in the field."
+            content="An enthusiastic full stack software engineer with 1 year of experience in the field."
           />
           <meta
             name="viewport"
             content="width=device-width,minimum-scale=1,initial-scale=1"
           />
           <meta property="og:type" content="website" />
-          <meta name="og:title" property="og:title" content="Kumar Deepanshu" />
+          <meta name="og:title" property="og:title" content="Nandish Tippashetty" />
           <meta
             name="og:description"
             property="og:description"
-            content="An enthusiastic front-end software engineer with more than 4 years of experience in the field."
+            content="An enthusiastic full stack software engineer with 1 year of experience in the field."
           />
           <meta
             property="og:site_name"
-            content="Kumar deepanshu Personal Portfolio"
+            content="Nandish Tippashetty Personal Portfolio"
           />
 
           <meta name="twitter:card" content="It is my personal portfolio" />
-          <meta name="twitter:title" content="Kumar Deepanshu" />
+          <meta name="twitter:title" content="Nandish Tippashetty" />
           <meta
             name="twitter:description"
-            content="An enthusiastic front-end software engineer with more than 4 years of experience in the field."
+            content="An enthusiastic full stack software engineer with 1 year of experience in the field."
           />
           <meta name="twitter:site" content="@propernoun" />
           <meta name="twitter:creator" content="@propernounco" />
 
-          <meta
+          {/* <meta
             property="og:image"
             content="https://portfolio-templte.pages.dev/ogImage.png"
-          />
-          <meta
+          /> */}
+          {/* <meta
             name="twitter:image"
             content="https://portfolio-templte.pages.dev/ogImage.png"
-          />
+          /> */}
 
           {/* Open Graph / Facebook */}
           <meta property="og:type" content="website" />
-          <meta
+          {/* <meta
             property="og:url"
             content="https://portfolio-templte.pages.dev/"
-          />
-          <meta property="og:title" content="Kumar Deepanshu" />
+          /> */}
+          <meta property="og:title" content="Nandish Tippashetty" />
           <meta
             property="og:description"
-            content="An enthusiastic front-end software engineer with more than 4 years of experience in the field."
+            content="An enthusiastic full stack software engineer with 1 year of experience in the field."
           />
-          <meta
+          {/* <meta
             property="og:image"
             content="https://portfolio-templte.pages.dev/ogImage.png"
-          />
-          <meta property="og:site_name" content="Kumar Deepanshu" />
+          /> */}
+          <meta property="og:site_name" content="Nandish Tippashetty" />
           {/* Twitter */}
           <meta property="twitter:card" content="summary_large_image" />
-          <meta
+          {/* <meta
             property="twitter:url"
             content="https://portfolio-templte.pages.dev/"
-          />
-          <meta property="twitter:title" content="Kumar Deepanshu" />
-          <meta
+          /> */}
+          <meta property="twitter:title" content="Nandish Tippashetty" />
+          {/* <meta
             property="twitter:description"
             content="An enthusiastic front-end software engineer with more than 4 years of experience in the field."
-          />
-          <meta
+          /> */}
+          {/* <meta
             property="twitter:image"
             content="https://portfolio-templte.pages.dev/ogImage.png"
-          />
+          /> */}
           <meta name="robots" content="Index" />
           <link rel="manifest" href="/manifest.json" />
 
